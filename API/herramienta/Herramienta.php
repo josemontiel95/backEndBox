92e979d0277409739c5e137607524041924a5c3f
Bryan <bryants.284@gmail.com>
2018-07-13 12:33:25 -0500

    Funciones de herramientas y validacion de email listo

--- API/herramienta/Herramienta.php
+++ API/herramienta/Herramienta.php
@@ -1,6 +1,7 @@
 <?php 
-
-class Herramienta(){
+include_once("./../../configSystem.php");
+include_once("./../../usuario/Usuario.php");
+class Herramienta{
 	private $id_herramienta;
 	private $herramienta_tipo_id;
 	private $fechaDeCompra;
@@ -10,23 +11,29 @@ class Herramienta(){
 	private $wc = '/1QQ/';
 
 
-	public function insert($herramienta_tipo_id,$fechaDeCompra,$condicion){
+	public function insert($token,$rol_usuario_id,$herramienta_tipo_id,$fechaDeCompra,$condicion){
 		global $dbS;
-		//Como mostrar el tipo de herramienta en el arreglo?
-		$dbS->squery("
+		$usuario = new Usuario();
+		$arr = json_decode($usuario->validateSesion($token, $rol_usuario_id),true);
+		if($arr['error'] == 0){
+			$dbS->squery("
 						INSERT INTO
 						herramientas(herramienta_tipo_id,fechaDeCompra,condicion)
 
 						VALUES
 						('1QQ','1QQ','1QQ')
 				",array($herramienta_tipo_id,$fechaDeCompra,$condicion),"INSERT");
-		$arr = array('id_herramienta' => 'No disponible, esto NO es un error', 'herramienta_tipo_id' => $herramienta_tipo_id, 'estatus' => 'Exito en insercion', 'error' => 0);
+			$arr = array('id_herramienta' => 'No disponible, esto NO es un error', 'herramienta_tipo_id' => $herramienta_tipo_id, 'estatus' => 'Exito en insercion', 'error' => 0);
+		}
 		return json_encode($arr);
 	}
 
-	public function upDate($id_herramienta,$herramienta_tipo_id,$fechaDeCompra,$condicion){
+	public function upDate($token,$rol_usuario_id,$id_herramienta,$herramienta_tipo_id,$fechaDeCompra,$condicion){
 		global $dbS;
-		$dbS->squery("	UPDATE
+		$usuario = new Usuario();
+		$arr = json_decode($usuario->validateSesion($token, $rol_usuario_id),true);
+		if($arr['error'] == 0){
+			$dbS->squery("	UPDATE
 							herramientas
 						SET
 							herramienta_tipo_id ='1QQ',
@@ -39,12 +46,16 @@ class Herramienta(){
 					,array($herramienta_tipo_id,$fechaDeCompra,$condicion,$id_herramienta),"UPDATE"
 			      	);
 		$arr = array('id_herramienta' => $id_herramienta, 'herramienta_tipo_id' => $herramienta_tipo_id,'estatus' => 'Exito de actualizacion','error' => 0);
+		}
 		return json_encode($arr);
 	}
 
-	public function deactivate($id_herramienta){
+	public function deactivate($token,$rol_usuario_id,$id_herramienta){
 		global $dbS;
-		$dbS->squery("	UPDATE
+		$usuario = new Usuario();
+		$arr = json_decode($usuario->validateSesion($token, $rol_usuario_id),true);
+		if($arr['error'] == 0){
+			$dbS->squery("	UPDATE
 							herramientas
 						SET
 							active = 1QQ
@@ -56,6 +67,7 @@ class Herramienta(){
 			      	);
 		//PENDIENTE por la herramienta_tipo_id para poderla imprimir tengo que cargar las variables de la base de datos?
 		$arr = array('id_herramienta' => $id_herramienta, 'herramienta_tipo_id' => $herramienta_tipo_id,'estatus' => 'Herramienta se desactivo','error' => 0);
+		}
 		return json_encode($arr);
 	}
 
---
+++ API/herramienta_tipo/Herramienta_tipo.php
@@ -0,0 +1,112 @@
+<?php 
+
+include_once("./../../configSystem.php");
+include_once("./../../usuario/Usuario.php");
+
+
+class Herramienta_tipo{
+	//Variables de BD
+	private $id_herramienta_tipo;
+	private $tipo;
+	private $placas;
+	/* Variables de utilería */
+	private $wc = '/1QQ/';
+
+
+
+	/*
+		Siguiendo la metodologia de POO las acciones las seguiria haciendo el usuario
+	*/
+
+	function getAll($token,$rol_usuario_id){
+		global $dbS;
+		$usuario = new Usuario();
+		$arr = json_decode($usuario->validateSesion($token, $rol_usuario_id),true);
+		if($arr['error'] == 0){
+			$arr= $dbS->qAll("
+			      SELECT 
+			        id_herramienta_tipo,
+					tipo
+			      FROM 
+			        Herramienta_tipo
+			      WHERE
+			      	 active = 1
+			      ",
+			      array(),
+			      "SELECT"
+			      );
+			if($arr = "empty") //Si esta bien el count porque si fuera un error la funcion devolveria 0 porque no lo ejecutaria correctamente
+				$arr = array('estatus' =>"No hay registros", 'error' => 1); //Pendiente
+		}
+		return json_encode($arr);	
+
+	}
+
+	public function insert($token,$rol_usuario_id,$tipo,$placas){
+		global $dbS;
+		$usuario = new Usuario();
+		$arr = json_decode($usuario->validateSesion($token, $rol_usuario_id),true);		
+		if($arr['error'] == 0){
+			$dbS->squery("
+						INSERT INTO
+						herramienta_tipo(tipo,placas)
+
+						VALUES
+						('1QQ','1QQ')
+				",array($tipo,$placas),"INSERT");
+			$arr = array('id_herramienta_tipo' => 'No dispinible, esto NO es un error','estatus' => 'Exito de insercion','error' => 0);
+		}
+		return json_encode($arr);
+	}
+
+
+	public function upDate($token,$rol_usuario_id,$id_herramienta_tipo,$tipo,$placas){
+		global $dbS;
+		$usuario = new Usuario();
+		$arr = json_decode($usuario->validateSesion($token, $rol_usuario_id),true);
+		if($arr['error'] == 0){
+			$dbS->squery("	UPDATE
+							herramienta_tipo
+						SET
+							tipo = '1QQ',
+							placas = '1QQ'
+						WHERE
+							active=1 AND
+							id_herramienta_tipo = 1QQ
+					 "
+					,array($tipo,$placas,$id_herramienta_tipo),"UPDATE"
+			      	);
+			$arr = array('id_herramienta_tipo' => $id_herramienta_tipo, 'tipo' => $tipo,'estatus' => 'Exito de actualizacion','error' => 0);
+		}
+		return json_encode($arr);
+	}
+
+	public function deactive($token,$rol_usuario_id,$id_herramienta_tipo){
+		global $dbS;
+		$usuario = new Usuario();
+		$arr = json_decode($usuario->validateSesion($token, $rol_usuario_id),true);
+		if($arr['error'] == 0){
+			$dbS->squery("	UPDATE
+							herramienta_tipo
+						SET
+							active = '1QQ'
+						WHERE
+							active=1 AND
+							id_herramienta_tipo = 1QQ
+					 "
+					,array(0,$id_herramienta_tipo),"UPDATE"
+			      	);
+			$arr = array('id_herramienta_tipo' => $id_herramienta_tipo,'estatus' => 'Herramienta_tipo desactivado','error' => 0);
+		}
+		return json_encode($arr);
+	}
+
+
+
+
+
+}
+
+
+
+?>
