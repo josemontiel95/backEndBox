7197fa098fa1e1ef462c265a1362bc9897d38e0f
Bryan <bryants.284@gmail.com>
2018-07-25 12:50:52 -0500

    SE realizaron modificaiones para la implementacion del campo observaciones en la clase herramientas

--- API/herramienta/post/endpoint.php
+++ API/herramienta/post/endpoint.php
@@ -17,11 +17,11 @@
 	switch ($function){
 		case 'insertAdmin':
 			$herra = new Herramienta();
-			echo $herra->insertAdmin($_POST['token'],$_POST['rol_usuario_id'],$_POST['herramienta_tipo_id'],$_POST['fechaDeCompra'],$_POST['placas'],$_POST['condicion']);		
+			echo $herra->insertAdmin($_POST['token'],$_POST['rol_usuario_id'],$_POST['herramienta_tipo_id'],$_POST['fechaDeCompra'],$_POST['placas'],$_POST['condicion'],$_POST['observaciones']);		
 		break;
 		case 'upDateAdmin':
 			$herra = new Herramienta();
-			echo $herra->upDateAdmin($_POST['token'],$_POST['rol_usuario_id'],$_POST['id_herramienta'],$_POST['herramienta_tipo_id'],$_POST['fechaDeCompra'],$_POST['placas'],$_POST['condicion']);
+			echo $herra->upDateAdmin($_POST['token'],$_POST['rol_usuario_id'],$_POST['id_herramienta'],$_POST['herramienta_tipo_id'],$_POST['fechaDeCompra'],$_POST['placas'],$_POST['condicion'],$_POST['observaciones']);
 		break;
 		case 'deactivate':
 			$herra_tipo = new Herramienta();
