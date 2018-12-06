3fa3a8d0028034565aba89574ab01b998f29633f
Mike <soto77mas@hotmail.com>
2018-12-05 18:10:35 -0600

    Mike

---
+++ API/scripts/databaseCleanUp/herramienta_tipo.sql
@@ -0,0 +1,53 @@
+-- INSERT INTO herramienta_tipo ==========================
+-- Author:		Miguel Angel Cesar Soto
+-- Create date: 20181204
+-- Last update: 20181204
+-- Release Date: 20181204
+-- Description:	
+--              INSERTS default values for clean database
+-- =============================================
+
+INSERT INTO herramienta_tipo (id_herramienta_tipo,tipo,asignableenOrdenDeTrabajo) VALUES
+(1001, "CONO DE REVENIMIENTO"           ,1),
+(1002, "VARILLAS DE COMPACTACION 300"   ,1),
+(1003, "FLEXOMETRO"            			,1),
+(1004, "TERMOMETRO BULBO SECO Y HUMEDO" ,1),
+(1005, "BASCULA DIGITAL"               	,0),
+(1006, "REGLA"                 			,1),
+(1007, "VERNIER"               			,1),
+(1008, "PRENSA"                			,0),
+(1009, "CUBO"                  			,1),
+(1010, "VIGAS"                  		,1),
+(1011, "MOLDE CILINDRICO"              	,1),
+(1012, "Prueba33"              			,1),
+(1013, "FORD PICKUP XL"        			,0),
+(1014, "PLACA PARA REVENIMIENTO"        ,1),
+(1016, "CUCHARON"              			,1),
+(1017, "MAZO DE HULE"                  	,1),
+(1019, "ENRASADOR"             			,1),
+(1020, "CARRETILLA"            			,1),
+(1021, "PALA"                  			,1),
+(1022, "CUCHARA DE ALBAÃ?IL"   			,1),
+(1023, "DISPOSITIVO PARA VIGAS"			,1),
+(1024, "CUBOS DE 10*10 CM"     			,1),
+(1025, "CUBOS DE 15*15 CM"     			,1),
+(1026, "MAZO DE HULE"   	   			,1),
+(1027, "DISPOSITIVO DE ALINEAMIENTO"    ,1),
+(1028, "PLATO DE CABECEO"               ,1),
+(1029, "CUBO DE AZUFRE"                 ,1),
+(1030, "VARILLAS DE COMPACTACION 600"   ,1),
+(1031, "TERMOMETRO DE LIQUIDO EN VIDRIO",1),
+(1032, "TERMOMETRO BIMETALICO"          ,1),
+(1033, "TERMOMETRO INFRARROJO DIGITAL"  ,1),
+(1034, "TERMOHIGROMETRO"  				,1),
+(1035, "BALANZA ANALOGA"  				,0),
+(1036, "BALANZA"  						,0),
+(1037, "COMPAS EXTERIORES"  			,1),
+(1038, "OLLA PARA FUNDIR AZUFRE"  		,1),
+(1039, "LAINAS"  						,1),
+(1040, "PALA"  							,1),
+(1041, "HUMIDIFICADOR"  				,1),
+(1042, "CARRETILLA"  					,1),
+(1043, "NIVELETA"  					    ,1);
+ 
+DELETE FROM herramienta_tipo;
