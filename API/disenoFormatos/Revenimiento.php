1da7052ab7c461aacc022d50c9cd21dfca492ff2
Bryan <bryants.284@gmail.com>
2018-12-19 10:23:19 -0600

    Se cambio la clave de validacion 'FI' por 'F1'

--- API/disenoFormatos/Revenimiento.php
+++ API/disenoFormatos/Revenimiento.php
@@ -1093,7 +1093,7 @@
 		    $this->Cell($tam_noPagina,10,utf8_decode($noPagina),0,0,'C');
 
 		    //Clave de validacion
-		    $clave = 'FI-02-LCC-02-0.1';
+		    $clave = 'F1-02-LCC-02-0.1';
 		    $tam_clave = $this->GetStringWidth($clave);
 		    $this->SetX(-($tam_clave + 10));
 		    $this->Cell($tam_noPagina,10,$clave,0,0,'C');
